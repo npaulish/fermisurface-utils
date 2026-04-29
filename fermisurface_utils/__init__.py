62de917236347dfc1ad66e5007363d3bf4177f7c
npaulish <nataliya.paulish@psi.ch>
2026-04-29 10:55:05 +0200

    Initialize project with basic structure and configuration files

---
+++ fermisurface_utils/__init__.py
@@ -0,0 +1,7 @@
+"""Fermisurface utilities package."""
+
+__version__ = "0.1.0"
+__author__ = "npaulish"
+
+# Import main utilities here as needed
+# from .module import function
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fermisurface-utils",
+    version="0.1.0",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
