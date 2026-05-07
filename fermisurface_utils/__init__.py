7097c9de7ed2d4a69746c8ca465adad2162a9a11
paulis_n <paulish98@mail.ru>
2026-05-07 21:56:16 +0200

    Add read_bxsf and write_bxsf utility functions to handle .bxsf files and update __all__ in __init__.py

--- fermisurface_utils/__init__.py
+++ fermisurface_utils/__init__.py
@@ -3,6 +3,7 @@
 __version__ = "0.1.0"
 __author__ = "npaulish"
 
+from .bxsf import read_bxsf, write_bxsf
 from .periodic import (
 	find_periodic_copy_groups,
 	find_unique_surfaces,
@@ -12,6 +13,8 @@ from .periodic import (
 )
 
 __all__ = [
+	"read_bxsf",
+	"write_bxsf",
 	"find_periodic_copy_groups",
 	"find_unique_surfaces",
 	"points_in_first_bz",
